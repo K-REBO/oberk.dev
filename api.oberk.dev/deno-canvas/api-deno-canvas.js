a7bfdc0a75ef11aeb21e82e5918c8338a6ccc210
Bido Nakamura <holmes10031208@gmail.com>
2021-11-03 21:14:54 +0900

    :arrow_up: upgrade

--- api.oberk.dev/deno-canvas/api-deno-canvas.js
+++ api.oberk.dev/deno-canvas/api-deno-canvas.js
@@ -1,5 +1,4 @@
 import { createCanvas } from "https://deno.land/x/canvas/mod.ts";
-import { listenAndServe } from "https://deno.land/std@0.111.0/http/server.ts";
 
 
 const screen = {height: 828,width: 1792,};
@@ -8,8 +7,8 @@ const ctx = canvas.getContext("2d");
 
 const exam = new Date("2022/1/15");
 
+addEventListener("fetch", async (evt) => {
 
-function handler() {
 	const font_size = 400;
 	ctx.fillStyle = "black";
 	const today = new Date();
@@ -25,9 +24,5 @@ function handler() {
 	ctx.fillStyle = "white";
 	ctx.fillText(`${day}`,screen.height/2 - Math.round(font_size/digit_length) - 50,screen.width * 4/5);
 
-	// await Deno.writeFileSync("image.png",canvas.toBuffer());
-	return new Response(canvas.toBuffer());
-}
-  
-console.log("Listening on http://localhost:8000");
-await listenAndServe(":8000", handler);
+	return new Response(canvas.toBuffer());  
+});
